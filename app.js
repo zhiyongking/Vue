2b1dba5696ad5474785a0d655d847f1eeaf94dbd
zhiyong <m18340006097@163.com>
2019-08-30 15:22:17 +0800

    'first'

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ new Vue({
     data:{
         name:"米斯特王",
         job:"web开发",
+        website:"zhiyongking.com",
+        websiteTag:"<a href='zhiyongking.com'>ZY</a>"
+    },
+    methods:{
+        greet: function(time){
+            return  'Good ' + time + "!" + " " +"Nice to meet you!" + " " + "My name is" + this.name;
+        }
     }
 });
